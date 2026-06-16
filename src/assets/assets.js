467ebdd1d3e038d098395b3d7d55e73f166fa0ac
ousmane <sangaryousmane@gmail.com>
2026-06-16 18:22:32 +0000

    Add additional packages

---
+++ src/assets/assets.js
@@ -0,0 +1,10 @@
+import logo from './logo.png';
+import header from './header.png';
+import logo_home from './logo_home.png';
+
+
+export const assets = {
+    logo: logo,
+    logo_home: logo_home,
+    header: header,
+}
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -16,7 +15,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
